23aa906ad66b48d1586e82266e7eca3011f4f176
pei guo <amazingguo@gmail.com>
2017-12-27 21:22:21 -0700

    dummy commit

--- torchvision/datasets/__init__.py
+++ torchvision/datasets/__init__.py
@@ -8,9 +8,11 @@ from .svhn import SVHN
 from .phototour import PhotoTour
 from .fakedata import FakeData
 from .semeion import SEMEION
+from .h5 import MultiTarget
 
 __all__ = ('LSUN', 'LSUNClass',
            'ImageFolder', 'FakeData',
+           'MultiTarget',
            'CocoCaptions', 'CocoDetection',
            'CIFAR10', 'CIFAR100', 'EMNIST', 'FashionMNIST',
            'MNIST', 'STL10', 'SVHN', 'PhotoTour', 'SEMEION')
